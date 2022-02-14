f6984169b82713b5036cd6d80702189d9b582f2b
Oles <doskucho@gmail.com>
2022-02-14 13:56:00 +0200

    add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO category(items, name, price)VALUES (12, 'Chocolate bar', 32.75),
+                                               (11, 'Donut', 29.5),
+                                               (30, 'Different', 20.5);
+
+
+
+INSERT  INTO sold_items(date, items, name, price) VALUES ('2022-02-14',1,'Chocolate bar',32.75),
+                                                         ('2022-02-14',1,'Donut',29.5),
+                                                         ('2022-02-13',1,'Different',20.5),
+                                                         ('2022-02-14',1,'Donut',29.5),
+                                                         ('2022-02-12',1,'Chocolate bar',32.75);
